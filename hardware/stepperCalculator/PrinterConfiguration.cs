4fe8eacb7bea2db6f77b1b22114904c321fa77c3
profesor79 <git[2~@profesor79.pl>
2018-11-02 23:08:17 +0000

    preparing move calculator

--- hardware/stepperCalculator/PrinterConfiguration.cs
+++ hardware/stepperCalculator/PrinterConfiguration.cs
@@ -5,10 +5,10 @@
         public float XStepsPerMM { get; set; } = 200;
         public float XMaxSpeedPerMM { get; set; } = 30;
 
-
         public float YStepsPerMM { get; set; }= 200;
         public float ZStepsPerMM { get; set; }= 200;
-        public int XMaxAcceleration { get; set; } = 60;
+        public int XMaxAcceleration { get; set; } = 3;
+        public int XJerk { get; set; } = 20;
         public int YMaxAcceleration { get; set; } = 60;
         public int ZMaxAcceleration { get; set; } = 60;
 
