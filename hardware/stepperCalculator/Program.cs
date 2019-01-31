9d5860ebbe41c4b0891ffb46b102a0b094fb5658
profesor79 <git[2~@profesor79.pl>
2019-01-31 22:49:05 +0000

    changes

--- hardware/stepperCalculator/Program.cs
+++ hardware/stepperCalculator/Program.cs
@@ -54,12 +54,91 @@ namespace stepperCalculator
             }
 
             Console.WriteLine($"TravelTimeX: {timeX}");
-            Console.WriteLine($"TravelTimeY: {timeY}");
+        Console.WriteLine($"TravelTimeY: {timeY}");
 
-            var steps = stepsX.BodySteps.Count+stepsX.HeadSteps.Count+stepsX.TailSteps.Count;
-            var steps2 = stepsY.BodySteps.Count+stepsY.HeadSteps.Count+stepsY.TailSteps.Count;
-            System.Console.WriteLine(steps);
-            System.Console.WriteLine(steps2);
+
+        Console.WriteLine($"Syncing times");
+   
+  if(timeX > timeY){
+var factor = timeX/timeY;
+
+ foreach (var step in stepsY.HeadSteps)
+            {
+                step.StepTime = factor * step.StepTime;
+            }
+
+            foreach (var step in stepsY.BodySteps)
+            {
+                 step.StepTime = factor * step.StepTime;
+            }
+
+            foreach (var step in stepsY.TailSteps)
+            {
+                 step.StepTime = factor * step.StepTime;
+
+            }
+
+
+  } else{
+
+  }
+// calclateTotalTime
+             timeX = 0.00;
+            foreach (var step in stepsX.HeadSteps)
+            {
+                timeX += step.StepTime;
+            }
+
+            foreach (var step in stepsX.BodySteps)
+            {
+                timeX += step.StepTime;
+            }
+
+            foreach (var step in stepsX.TailSteps)
+            {
+                timeX += step.StepTime;
+
+            }
+
+
+           timeY = 0.00;
+
+            foreach (var step in stepsY.HeadSteps)
+            {
+                timeY += step.StepTime;
+            }
+
+            foreach (var step in stepsY.BodySteps)
+            {
+                timeY += step.StepTime;
+            }
+
+            foreach (var step in stepsY.TailSteps)
+            {
+                timeY += step.StepTime;
+
+            }
+
+            Console.WriteLine($"TravelTimeX: {timeX}");
+            Console.WriteLine($"TravelTimeY: {timeY}");
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
             Console.WriteLine("done");
         }
     }
